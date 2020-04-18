43b883f5eb300f5f75195cbf7fb343a0064030b3
Simas <simas@simas-dei.com>
2020-04-18 18:23:00 +0300

    add comment with primitive types

--- src/com/training/Main.java
+++ src/com/training/Main.java
@@ -3,6 +3,21 @@ package com.training;
 public class Main {
 
     public static void main(String[] args) {
+        /*
+            Primitive Types:
+            Type    Byte    Range
+
+            byte    1   [-128, 127]
+            short   2   [-32k, 32k]
+            int     4   [-2B, 2B]
+            long    8
+            float   4
+            double  8
+
+            char    2   A,B,C
+            boolean 1   true/false
+
+         */
         System.out.println("Ahoy sailor o/ ⛵️");
     }
 }
